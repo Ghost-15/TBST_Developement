6c53ce495fc3c1095696b5745074b40634c36dbe
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-04-04 00:55:03 +0100

    Delete src directory

--- src/app/api/api.js
+++
@@ -1,9 +0,0 @@
-export const sendContactForm = async (data) =>
-    fetch("/api/hello", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json", Accept: "application/json" },
-    }).then((res) => {
-        if (!res.ok) throw new Error("Sorry, We are facing a technical problem.. please send the email by your mailbox");
-        return res.json();
-    });
