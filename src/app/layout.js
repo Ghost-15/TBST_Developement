f852f42b75494f585a53497d93c9b70c1e4c6dd4
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-04-04 19:52:49 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
       <html lang="en">
       <head>
           <meta charSet="UTF-8"/>
-          <link rel="icon" href="/picture.jpg"/>
+          <link rel="icon" href="/public/picture.jpg"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </head>
       <body>
