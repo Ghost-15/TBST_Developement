8d3c26d8999dc117821593073d63195fdcfaacd3
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-04-04 21:31:17 +0100

    Update Navbar.jsx

--- src/app/compoments/Navbar.jsx
+++ src/app/compoments/Navbar.jsx
@@ -1,3 +1,4 @@
+import Image1 from '../public/picturepng'
 import Image from "next/image";
 
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
         <nav className="bg-white border-gray-200">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
                 <a href="/TBST_Developement/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <Image src="/picture.jpg" width={50} height={50} alt="Logo"/>
+    <Image src={Image1} width='300px' height='200px' alt='this is my image'/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
                         TBST Development</span>
                 </a>
