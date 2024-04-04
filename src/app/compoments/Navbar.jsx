8f52cd8999caf75adac6ecd84301d0a2ffa8f41f
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-04-04 19:51:47 +0100

    Update Navbar.jsx

--- src/app/compoments/Navbar.jsx
+++ src/app/compoments/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
     return (
         <nav className="bg-white border-gray-200">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/picture.jpg" className="h-12" alt="Logo"/>
+                <a href="/ghost-15.github.io/TBST_Developement/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="/public/picture.jpg" className="h-12" alt="Logo"/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
                         TBST Development</span>
                 </a>
