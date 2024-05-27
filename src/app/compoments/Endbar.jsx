c79e61cd4db3acad1db8064f763b02fdf9dac761
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-05-27 18:19:57 +0100

    Update Endbar.jsx

--- src/app/compoments/Endbar.jsx
+++ src/app/compoments/Endbar.jsx
@@ -5,14 +5,14 @@ function Endbar() {
         <footer className="items-center justify-center bg-gray-900 mt-8 p-10">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <a href="/TBST_Developement" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <img src="/picture.jpg" className="h-8" alt="Logo"/>
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                             TBST Development</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
                         <li>
-                            <a href="/ghost-15.github.io/TBST_Developement/a-propos" className="hover:underline me-4 md:me-6">A propos</a>
+                            <a href="/TBST_Developement/a-propos" className="hover:underline me-4 md:me-6">A propos</a>
                         </li>
                         <li>
                             <a href="/TBST_Developement/contact" className="hover:underline">Contact</a>
