b2a3bfc9f5ddf92e68a38c6cbb041b420cb15363
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-05-27 18:55:01 +0100

    Update Endbar.jsx

--- src/app/compoments/Endbar.jsx
+++ src/app/compoments/Endbar.jsx
@@ -5,8 +5,8 @@ function Endbar() {
         <footer className="items-center justify-center bg-gray-900 mt-8 p-10">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="/TBST_Developement/public" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="/picture.jpg" className="h-8" alt="Logo"/>
+                    <a href="/TBST_Developement" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <img src="/TBST_Developement/public/picture.jpg" className="h-8" alt="Logo"/>
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                             TBST Development</span>
                     </a>
