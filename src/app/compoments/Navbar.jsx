dcf843cf8d54aa5562ee23c01a08bb849b531e07
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-05-27 18:33:28 +0100

    Update Navbar.jsx

--- src/app/compoments/Navbar.jsx
+++ src/app/compoments/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="bg-white border-gray-200">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-                <a href="/TBST_Developement/public" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="/public/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Image src="/picture.jpg" width={50} height={50} alt="Logo"/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
                         TBST Development</span>
