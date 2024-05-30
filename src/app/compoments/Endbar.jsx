4fb9eb5db833be6c936d9e7e0c7578ecb4e261a3
Ŧįm ŤǞŢĬ βѦϯϾӁӀ <83399865+Ghost-15@users.noreply.github.com>
2024-05-30 02:32:49 +0100

    Update Endbar.jsx

--- src/app/compoments/Endbar.jsx
+++ src/app/compoments/Endbar.jsx
@@ -2,7 +2,7 @@
 function Endbar() {
     return (
 
-        <footer className="items-center justify-center bg-gray-900 mt-8 p-10">
+        <footer className="items-center justify-center bg-gray-900 p-10">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="/TBST_Developement" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
